48c9d10f90f6a940fb47eae3ce19a02b9f65fa93
saurabh0809 <saurabhdeshpande123@gmail.com>
2019-02-02 19:43:43 +1100

    "adding fonts"

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="container">
+        <div className="row">
+          <div className="col-6">Column One</div>
+          <div className="col-6">
+            <span>
+              <i className="fas fa-home" />
+            </span>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
