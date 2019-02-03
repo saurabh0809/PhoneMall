204ed2e7c2bff64f3e1158ef67e7a0cf29a06622
saurabh0809 <saurabhdeshpande123@gmail.com>
2019-02-03 19:02:48 +1100

    adding NavWrapper css to navbar

---
+++ src/components/NavWrapper.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+// use css to apply to nav bar
+export const NavWrapper = styled.nav`
+  background: var(--mainBlue);
+  .nav-link {
+    color: var(--mainWhite) !important;
+    font-size: 1.3rem;
+    text-transform: capitalize;
+  }
+`;
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,14 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import logo from "../logo.svg";
+import styled from "styled-components";
 import { ButtonContainer } from "./Button";
+import { NavWrapper } from "./NavWrapper";
 
 export default class Navbar extends Component {
   render() {
     return (
-      <nav className="navbar navbar-expand-sm bg-primary navbar-dark px-sm-5">
+      <NavWrapper className="navbar navbar-expand-sm navbar-dark px-sm-5">
         {/*https://www.iconfinder.com/icons/1243689/call_phone_icon
           Creative Commons (Attribution 3.0 Unported);
           https://www.iconfinder.com/Makoto_msk */}
@@ -29,7 +31,7 @@ export default class Navbar extends Component {
             My Cart
           </ButtonContainer>
         </Link>
-      </nav>
+      </NavWrapper>
     );
   }
 }
