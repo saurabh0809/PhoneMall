2c535f326b69d14ef6298e02221308a69fe512ec
saurabh0809 <saurabhdeshpande123@gmail.com>
2019-02-03 19:23:08 +1100

    add products from data to productlist as a (STATE)

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,12 +1,27 @@
 import React, { Component } from "react";
 import Product from "./Product";
+import Title from "./Title";
+import { storeProducts } from "../data";
 
 export default class ProductList extends Component {
+  // getting state of products from data.js
+  state = {
+    products: storeProducts
+  };
+
   render() {
+    console.log(this.state.products);
+
     return (
-      <div>
-        <Product />
-      </div>
+      <React.Fragment>
+        <div className="py-5">
+          <div className="container">
+            <Title name="our" title="products" />
+
+            <div className="row" />
+          </div>
+        </div>
+      </React.Fragment>
     );
   }
 }
